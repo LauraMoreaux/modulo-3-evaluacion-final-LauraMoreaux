3320d68066d2e0f79f5c18c70ed6018514198e01
LauraMoreaux <lauramoreauxcueto@gmail.com>
2020-08-07 12:51:51 +0200

    Add run docs

---
+++ docs/precache-manifest.9f1ea7524583063ff3f7e78c731de18b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000c704baca8da7700bae46c3ccc7285",
+    "url": "./index.html"
+  },
+  {
+    "revision": "80ad4e3f78b0098918b4",
+    "url": "./static/css/main.bdb7edad.chunk.css"
+  },
+  {
+    "revision": "9a35ccfcf8e7a6dd287d",
+    "url": "./static/js/2.407158e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.407158e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ad4e3f78b0098918b4",
+    "url": "./static/js/main.f70945e4.chunk.js"
+  },
+  {
+    "revision": "99f77fec555d33d469c5",
+    "url": "./static/js/runtime-main.8e50b552.js"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "./static/media/get_schwifty.4b4420cf.ttf"
+  },
+  {
+    "revision": "5f38ccc5c62afc77b5de87ad646c3e9b",
+    "url": "./static/media/rick-and-morty-falling-out.5f38ccc5.jpg"
+  },
+  {
+    "revision": "d98644186fdf3d5dfc64349ba6568713",
+    "url": "./static/media/rick1.d9864418.jpg"
+  }
+]);
